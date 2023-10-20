438d5a924fc544ce55f9c0ff0deaa83b4faca9e8
Rin Iwai <iwai.rin.ij@tut.jp>
2023-10-20 08:35:27 +0000

    add talk feat

--- src/raspimouse_joy/raspimouse_joy/raspimouse_joy.py
+++ src/raspimouse_joy/raspimouse_joy/raspimouse_joy.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
+from aquestalkpi_ros_msgs.msg import Talk # /aquestalkpi_ros
 
 class RaspimouseJoy(Node):
     def __init__(self):
@@ -13,16 +14,20 @@ class RaspimouseJoy(Node):
             10
         )
         self.publisher =self.create_publisher(Twist,'/cmd_vel',10) 
+        
+        self.talker =self.create_publisher(Talk,'/aquestalkpi_ros',10)
+        
+        
 
     def listener_callback(self, joy):
-    	dec = 0.1
         twist = Twist()
-        if abs(joy.axes[0]>0.1)
+        twist.linear.x = 0.2 * joy.axes[1]
+        twist.angular.z = 2 * joy.axes[0]
         
-        	twist.linear.x = 0.2 * joy.axes[1]
-        	twist.angular.z = 2 * joy.axes[0]
-        	joy.axes[0] = joy.axes[0] - dec
-        	dec-=0.1
+        if joy.buttons[3]:
+            talk = Talk()
+            talk.text = "まってー"
+            self.talker.publish(talk)
 
         	
         self.publisher.publish(twist)
