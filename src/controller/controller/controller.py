47c5d8690d5913addd38059d8b2dc0e6043875e8
robotexp-group3 <robotexp-group3@eyrin.jp>
2023-11-10 16:19:46 +0900

    iikanji

--- src/controller/controller/controller.py
+++ src/controller/controller/controller.py
@@ -4,6 +4,10 @@ from geometry_msgs.msg import Twist
 from rclpy.node import Node
 
 from nanodet_ros_msgs.msg import BBox, BBoxArray
+from raspimouse_msgs.msg import LightSensors
+
+def clamp(x, l, h):
+    return min(max(x, l), h)
 
 
 class Controller(Node):
@@ -13,27 +17,55 @@ class Controller(Node):
         self.publisher = self.create_publisher(Twist, "cmd_vel", 10)
 
         self.talker = self.create_publisher(Talk, "aquestalkpi_ros/talk", 10)
+        
+        self.light_sensor_sub = self.create_subscription(LightSensors, "light_sensors",self.sensor_callback, 10)
+        
+        self.twist = Twist()
+        self.found = False
 
     def listener_callback(self, bboxarr):
         maxscore = 0
         xmin = 160
         xmax = 160
+        self.found = False
         for bbox in bboxarr.bboxes:
             if bbox.name == "person":
+                self.found = True
                 if bbox.score > maxscore:
                     maxscore = bbox.score
                     xmin = bbox.xmin
                     xmax = bbox.xmax
 
         center = (xmin + xmax) / 2
-        twist = Twist()
-        if center < 150:
-            twist.angular.z = 0.3  # * j
-        if center > 170:
-            twist.angular.z = -0.3  # * j
+        if center < 100:
+            self.twist.angular.z = 0.1  # * j
+        elif center > 220:
+            self.twist.angular.z = -0.1  # * j
+        else:
+            self.twist.angular.z = 0.0  # * j
+        
 
-        self.publisher.publish(twist)
+       
 
+
+    def sensor_callback(self, msg):
+        
+        d = clamp((msg.forward_r + msg.forward_l) / 2, 1, 500)
+        
+        if self.found:
+            if d >= 100: # self.twist.angular.z == 0.0 or
+                self.twist.linear.x = 0.0
+            elif 50 <= d and d <100:
+                self.twist.linear.x = 1 / d
+            else:
+                self.twist.linear.x = 0.02
+        else:
+            self.twist.linear.x = 0.0
+            #self.twist.angular.z = 0.2
+        
+        
+        
+        self.publisher.publish(self.twist)
 
 def main():
     rclpy.init()
@@ -45,3 +77,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
