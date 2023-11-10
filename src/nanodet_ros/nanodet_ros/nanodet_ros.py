ff8f109010fb0b8ae800e4e030914688be88c6e6
Rin Iwai <rin@eyrin.jp>
2023-11-10 01:44:16 +0000

    nanodet_rosに変更

--- src/nanodet_ros/nanodet_ros/nanodet_ros.py
+++ src/nanodet_ros/nanodet_ros/nanodet_ros.py
@@ -1,35 +1,31 @@
-import rclpy, cv2                                       
-from rclpy.node import Node
-from bbox_msgs.msg import BBox
-from bbox_msgs.msg import BBoxArray
+import cv2
+import rclpy
 from cv_bridge import CvBridge
+from nanodet.util import cfg, load_config
+from rclpy.node import Node
 from sensor_msgs.msg import Image
 
-from nanodet.util import cfg, load_config
+from nanodet_ros_msgs.msg import BBox, BBoxArray
+
 from .predictor import Predictor
+
 
 class BBoxesGet(Node):
     def __init__(self):
-        super().__init__('bboxes_get')
+        super().__init__("nanodet_ros")
         self.bridge = CvBridge()
-        self.subscription = self.create_subscription(
-            Image,
-            '/image_raw',
-            self.camera_callback,
-            1
-        )
+        self.subscription = self.create_subscription(Image, "/image_raw", self.camera_callback, 1)
 
         load_config(cfg, "/home/group3/nanodet/config/legacy_v0.x_configs/nanodet-m.yml")
         self.predictor = Predictor(cfg, "/home/group3/nanodet_m.ckpt")
-        
-        self.publisher = self.create_publisher(BBoxArray, 'bbox_data',10) 
-        
-        
+
+        self.publisher = self.create_publisher(BBoxArray, "bbox_data", 10)
+
     def camera_callback(self, msg):
         frame = self.bridge.imgmsg_to_cv2(msg)
         res = self.predictor.inference(frame)
-        #self.predictor.visualize(res[0], frame, cfg.class_names, 0.3)
-        #cv2.waitKey(1)
+        # self.predictor.visualize(res[0], frame, cfg.class_names, 0.3)
+        # cv2.waitKey(1)
         bboxes = BBoxArray()
         dets = res[0]
         for label in dets:
@@ -47,14 +43,15 @@ class BBoxesGet(Node):
                     bboxmsg.ymax = bbox[3]
                     bboxes.bboxes.append(bboxmsg)
         self.publisher.publish(bboxes)
-        
+
+
 def main():
     rclpy.init()
     node = BBoxesGet()
-    #irospy.Subscriber('/camera/depth/image_raw', Image, camera_callback)
     rclpy.spin(node)
     rclpy.destroy_node()
     rclpy.shutdown()
 
+
 if __name__ == "__main__":
-    main ()
+    main()
