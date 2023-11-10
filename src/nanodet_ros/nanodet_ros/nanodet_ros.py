068d7b25f97f8059f175380cc956089d8427cd08
Rin Iwai <rin@eyrin.jp>
2023-11-10 14:28:17 +0900

    compressed image

--- src/nanodet_ros/nanodet_ros/nanodet_ros.py
+++ src/nanodet_ros/nanodet_ros/nanodet_ros.py
@@ -1,10 +1,10 @@
 import cv2
 import rclpy
 from cv_bridge import CvBridge
-from nanodet.util import cfg, load_config
 from rclpy.node import Node
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
 
+from nanodet.util import cfg, load_config
 from nanodet_ros_msgs.msg import BBox, BBoxArray
 
 from .predictor import Predictor
@@ -14,7 +14,7 @@ class NanodetRos(Node):
     def __init__(self):
         super().__init__("nanodet_ros")
         self.bridge = CvBridge()
-        self.subscription = self.create_subscription(Image, "image_raw", self.camera_callback, 1)
+        self.subscription = self.create_subscription(CompressedImage, "image_raw/compressed", self.camera_callback, 1)
 
         load_config(cfg, "./nanodet/config/legacy_v0.x_configs/nanodet-m.yml")
         self.predictor = Predictor(cfg, "./nanodet/nanodet_m.ckpt")
@@ -22,7 +22,7 @@ class NanodetRos(Node):
         self.publisher = self.create_publisher(BBoxArray, "~/bboxes", 10)
 
     def camera_callback(self, msg):
-        frame = self.bridge.imgmsg_to_cv2(msg)
+        frame = self.bridge.compressed_imgmsg_to_cv2(msg)
         res = self.predictor.inference(frame)
         # self.predictor.visualize(res[0], frame, cfg.class_names, 0.3)
         # cv2.waitKey(1)
