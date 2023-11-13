3f246edee358c447c924e1d79f7544202e108822
Rin Iwai <rin@eyrin.jp>
2023-11-13 10:37:58 +0900

    少し修正

--- src/controller/setup.py
+++ src/controller/setup.py
@@ -1,28 +1,23 @@
-import os
-from glob import glob
-
-from setuptools import setup
+from setuptools import find_packages, setup
 
 package_name = "controller"
 
 setup(
     name=package_name,
     version="0.0.0",
-    packages=[package_name],
+    packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
-    maintainer="ubuntu",
-    maintainer_email="ubuntu@todo.todo",
+    maintainer="rin",
+    maintainer_email="rin@eyrin.jp",
     description="TODO: Package description",
     license="TODO: License declaration",
     tests_require=["pytest"],
     entry_points={
-        "console_scripts": [
-            "controller = controller.controller:main",
-        ],
+        "console_scripts": ["controller = controller.controller:main"],
     },
 )
